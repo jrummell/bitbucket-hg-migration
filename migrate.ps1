3ba48a99f62b9317489f652d00a04c33265b23ce
John Rummell <john@rummellweb.com>
2019-08-20 23:05:21 -0400

    added comments to scripts

--- migrate.ps1
+++ migrate.ps1
@@ -1,8 +1,10 @@
 param([string]$BitbucketAccount, [string] $Repository)
 
+# create variables for the git and hg repository folder names
 $gitRepo = "$Repository-git"
 $hgRepo = "$Repository-hg"
 
+# create a folder for the given account
 $accoutFolderExists = Test-Path $BitbucketAccount
 if (!$accoutFolderExists)
 {
@@ -11,14 +13,18 @@ if (!$accoutFolderExists)
 
 Set-Location $BitbucketAccount
 
+# create a new bare git repository
 git init --bare $gitRepo
 
+# clone the hg repository from Bitbucket
 hg clone ssh://hg@bitbucket.org/$BitbucketAccount/$Repository $hgRepo
 
 Set-Location $hgRepo
 
+# create an hg branch
 hg bookmarks hg
 
+# push the hg repository to the local git repository
 hg push ../$gitRepo
 
 Set-Location ..\..
--- push.ps1
+++ push.ps1
@@ -4,6 +4,7 @@ $gitRepo = "$Repository-git"
 
 Set-Location $BitbucketAccount/$gitRepo
 
+# push the local git repository to Bitbucket
 git remote add origin git@bitbucket.org:$BitbucketAccount/$Repository.git
 git push --all origin
 
